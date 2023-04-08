01893212c0beedd95d8845b3f577559466c48acb
abnerpersio <binhopersio@gmail.com>
2023-04-08 11:55:34 -0300

    chore: add prisma to project

---
+++ prisma/migrations/20230408145437_create_common_tables/migration.sql
@@ -0,0 +1,66 @@
+-- CreateEnum
+CREATE TYPE "GuestType" AS ENUM ('common', 'honor');
+
+-- CreateEnum
+CREATE TYPE "InviteStatus" AS ENUM ('waiting', 'confirmed', 'refused');
+
+-- CreateEnum
+CREATE TYPE "NotificationStatus" AS ENUM ('failed', 'delivered', 'pending');
+
+-- CreateEnum
+CREATE TYPE "MessageChannel" AS ENUM ('whatsapp', 'email');
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "guest" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" "GuestType" NOT NULL DEFAULT 'common',
+    "phone" TEXT,
+    "email" TEXT,
+
+    CONSTRAINT "guest_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "invite" (
+    "id" SERIAL NOT NULL,
+    "status" "InviteStatus" NOT NULL DEFAULT 'waiting',
+    "companions" INTEGER NOT NULL DEFAULT 0,
+    "commments" TEXT,
+    "guestId" INTEGER NOT NULL,
+
+    CONSTRAINT "invite_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "notification" (
+    "id" SERIAL NOT NULL,
+    "recipient" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "channel" "MessageChannel" NOT NULL,
+    "status" "NotificationStatus" NOT NULL DEFAULT 'pending',
+    "type" TEXT NOT NULL,
+    "failedReason" TEXT,
+    "guestId" INTEGER NOT NULL,
+
+    CONSTRAINT "notification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- AddForeignKey
+ALTER TABLE "invite" ADD CONSTRAINT "invite_guestId_fkey" FOREIGN KEY ("guestId") REFERENCES "guest"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "notification" ADD CONSTRAINT "notification_guestId_fkey" FOREIGN KEY ("guestId") REFERENCES "guest"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
