af0974339cf4e3a6295eb9ae2cceae1def882d16
abnerpersio <binhopersio@gmail.com>
2023-05-28 16:48:16 -0300

    chore: hash script

---
+++ scripts/createHash.ts
@@ -0,0 +1,11 @@
+import { Hash } from '../src/shared/utils/hash';
+
+const HASH_INPUT = process.env.HASH_INPUT || '';
+
+if (!HASH_INPUT) {
+  throw new Error('HASH_INPUT variable is required!');
+}
+
+const output = Hash.encrypt(HASH_INPUT);
+console.info('Shhhhh.....');
+console.info(`Hash result: ${output}`);
