dcec3aa6b16f79d3a47b51f2cd73a730a8290915
abnerpersio <binhopersio@gmail.com>
2023-06-04 17:00:19 -0300

    feat: remove venom from code

--- src/services/whatsapp/venom/index.ts
+++ src/services/whatsapp/venom/index.ts
@@ -1,28 +1,26 @@
-import * as venom from 'venom-bot';
+// import * as venom from 'venom-bot';
 import { WhatsappClient } from '~/services/whatsapp/types';
 import { Formatters } from '~/shared/utils/formatters';
 
 export class VenomClientAdapter implements WhatsappClient {
-  private client?: venom.Whatsapp;
+  // private client?: venom.Whatsapp;
 
   async build() {
-    const options = {
-      logQR: true,
-    } satisfies venom.CreateConfig;
-    this.client = await venom.create('wedding-api-session', undefined, undefined, options);
+    // const options = {
+    //   logQR: true,
+    // } satisfies venom.CreateConfig;
+    // this.client = await venom.create('wedding-api-session', undefined, undefined, options);
     return this;
   }
 
   async sendMessage(message: string, phone: string) {
-    if (!this.client) {
-      throw new Error('Client not configured');
-    }
-
-    const whatsappId = Formatters.whatsappId(phone);
-    if (!whatsappId) {
-      throw new Error('Invalid whatsapp ID');
-    }
-
-    await this.client.sendText(whatsappId, message);
+    // if (!this.client) {
+    //   throw new Error('Client not configured');
+    // }
+    // const whatsappId = Formatters.whatsappId(phone);
+    // if (!whatsappId) {
+    //   throw new Error('Invalid whatsapp ID');
+    // }
+    // await this.client.sendText(whatsappId, message);
   }
 }
